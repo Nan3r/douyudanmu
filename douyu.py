43cfb0ae3bc51f7f2d0d24b85447eda2fccc173b
Nan3r <nan3r@foxmail.com>
2017-06-04 13:07:47 +0800

    斗鱼弹幕

---
+++ douyu.py
@@ -0,0 +1,23 @@
+#encoding:utf-8
+from douyu.chat.room import ChatRoom
+import time,os,MySQLdb
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def on_chat_message(msg):
+	os.environ['TZ']='Asia/Shanghai'
+	#time.tzset()
+	msg_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+	user = msg.attr('nn')
+	txt = msg.attr('txt')
+	level = msg.attr('level')
+	print "[%s] [lv%s %s]:%s" % (msg_time,level,user,txt)
+
+def run():
+	room = ChatRoom(sys.argv[1])
+	room.on('chatmsg', on_chat_message)
+	room.knock()
+
+if __name__ == '__main__':
+	run()
